1571d0b411f7e5e6054f157cd03cf51d49fb4d10
sihiep <sihuynh1988@gmail.com>
2015-01-24 23:07:38 +0700

    update path of data file

--- Getting_Clean_Data/Course_Project/run_analysis.R
+++ Getting_Clean_Data/Course_Project/run_analysis.R
@@ -1,26 +1,25 @@
 
 # Get and clean data
 
-
 function run_analysis() {
   # Read features.txt
-  features <- read.table("features.txt")
+  features <- read.table("getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/features.txt")
   # Convert to name
   listOfNames <- make.names(features[,2], unique = T)
   # Find element contain mean or std
   measurements <- grep("mean[^Freq]|std", listOfNames)
   # Read train data
-  trainXData <- read.table("train/X_train.txt")
+  trainXData <- read.table("getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/X_train.txt")
   trainXData <- trainXData[, measurements]
-  trainYData <- read.table("train/y_train.txt")
-  subjectTrainData <- read.table("train/subject_train.txt")
+  trainYData <- read.table("getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/y_train.txt")
+  subjectTrainData <- read.table("getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/subject_train.txt")
   # Merge train data into one
   trainData <- cbind(subjectTrainData, trainYData, trainXData)
   # Read test data
-  testXData <- read.table("test/X_test.txt")
+  testXData <- read.table("getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/X_test.txt")
   testXData <- testXData[, measurements]
-  testYData <- read.table("test/y_test.txt")
-  subjectTestData <- read.table("test/subject_test.txt")
+  testYData <- read.table("getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/y_test.txt")
+  subjectTestData <- read.table("getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/subject_test.txt")
   # Merge test data into one
   testData <- cbind(subjectTestData, testYData, testXData)
   # Merge train and test into XYData
@@ -28,14 +27,14 @@ function run_analysis() {
   # set names for XYData
   names(XYData) <- c("subject", "activity", listOfNames[measurements])
   # Read activity
-  label_activity <- read.table("activity_labels.txt")
+  label_activity <- read.table("getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/activity_labels.txt")
   # set names for activity
   names(label_activity) <- c("activity", "description")
   # Merge XYData and activity to know relation XYData and activity clearly
   finalData <- merge(label_activity, XYData, by = "activity")
   # Remove redundant activity column, because this column is numberic is not meaningful
   finalData <- finalData[, 2:69]
-  
+  # the average of each variable for each activity and each subject
   finalData <- aggregate(.~ subject + description, finalData, mean)
 }
 
