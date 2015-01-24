218d71ec3992be99cd11b1ba6a0de20b83530c32
sihiep <sihuynh1988@gmail.com>
2015-01-24 23:01:12 +0700

    update comments for script

--- Getting_Clean_Data/Course_Project/run_analysis.R
+++ Getting_Clean_Data/Course_Project/run_analysis.R
@@ -1,41 +1,44 @@
 
-features <- read.table("features.txt")
+# Get and clean data
 
-listOfNames <- make.names(features[,2], unique = T)
 
-measurements <- grep("mean[^Freq]|std", listOfNames)
+function run_analysis() {
+  # Read features.txt
+  features <- read.table("features.txt")
+  # Convert to name
+  listOfNames <- make.names(features[,2], unique = T)
+  # Find element contain mean or std
+  measurements <- grep("mean[^Freq]|std", listOfNames)
+  # Read train data
+  trainXData <- read.table("train/X_train.txt")
+  trainXData <- trainXData[, measurements]
+  trainYData <- read.table("train/y_train.txt")
+  subjectTrainData <- read.table("train/subject_train.txt")
+  # Merge train data into one
+  trainData <- cbind(subjectTrainData, trainYData, trainXData)
+  # Read test data
+  testXData <- read.table("test/X_test.txt")
+  testXData <- testXData[, measurements]
+  testYData <- read.table("test/y_test.txt")
+  subjectTestData <- read.table("test/subject_test.txt")
+  # Merge test data into one
+  testData <- cbind(subjectTestData, testYData, testXData)
+  # Merge train and test into XYData
+  XYData <- rbind(trainData, testData)
+  # set names for XYData
+  names(XYData) <- c("subject", "activity", listOfNames[measurements])
+  # Read activity
+  label_activity <- read.table("activity_labels.txt")
+  # set names for activity
+  names(label_activity) <- c("activity", "description")
+  # Merge XYData and activity to know relation XYData and activity clearly
+  finalData <- merge(label_activity, XYData, by = "activity")
+  # Remove redundant activity column, because this column is numberic is not meaningful
+  finalData <- finalData[, 2:69]
+  
+  finalData <- aggregate(.~ subject + description, finalData, mean)
+}
 
-trainXData <- read.table("train/X_train.txt")
 
-trainXData <- trainXData[, measurements]
 
-trainYData <- read.table("train/y_train.txt")
-
-subjectTrainData <- read.table("train/subject_train.txt")
-
-trainData <- cbind(subjectTrainData, trainYData, trainXData)
-
-testXData <- read.table("test/X_test.txt")
-
-testXData <- testXData[, measurements]
-
-testYData <- read.table("test/y_test.txt")
-
-subjectTestData <- read.table("test/subject_test.txt")
-
-testData <- cbind(subjectTestData, testYData, testXData)
-
-allData <- rbind(trainData, testData)
-
-names(allData) <- c("subject", "activity", listOfNames[measurements])
-
-label_activity <- read.table("activity_labels.txt")
-
-names(label_activity) <- c("activity", "description")
-
-finalData <- merge(label_activity, allData, by = "activity")
-
-finalData <- finalData[, 2:69]
-
-finalData <- aggregate(.~ subject + description, finalData, mean)
 
